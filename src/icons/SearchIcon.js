0bc6f1e1b7f0f2d39e53125f2e8c42bb4c9b9b27
Carlos Aviña <juan.avina@wizeline.com>
2022-03-22 23:42:37 -0600

    Create SearchIcon

---
+++ src/icons/SearchIcon.js
@@ -0,0 +1,20 @@
+const SearchIcon = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={68}
+    height={68}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="#000"
+    strokeWidth={2.5}
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="feather feather-search"
+    {...props}
+  >
+    <circle cx={11} cy={11} r={8} />
+    <path d="m21 21-4.35-4.35" />
+  </svg>
+);
+
+export default SearchIcon;
